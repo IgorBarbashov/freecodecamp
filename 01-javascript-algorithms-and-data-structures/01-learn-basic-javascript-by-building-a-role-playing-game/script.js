a11884d2f25de94ee976af79632f2a3ea06a972d
Igor Barbashov <bis200705@gmail.com>
2024-04-01 11:03:35 +0300

    [role-playing-game] - adds html markup and variables (steps 30-42)

--- 01-javascript-algorithms-and-data-structures/01-learn-basic-javascript-by-building-a-role-playing-game/script.js
+++ 01-javascript-algorithms-and-data-structures/01-learn-basic-javascript-by-building-a-role-playing-game/script.js
@@ -7,5 +7,28 @@ let monsterHealth;
 let inventory = ["stick"];
 
 const button1 = document.querySelector("#button1");
+button1.onclick = goStore;
 const button2 = document.querySelector("#button2");
+button2.onclick = goCave;
 const button3 = document.querySelector("#button3");
+button3.onclick = fightDragon;
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+}
+
+function goCave() {
+    button2.innerText = "Buy weapon (30 gold)";
+}
+
+function fightDragon() {
+    button3.innerText = "Go to town square";
+}
